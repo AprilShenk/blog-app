ed02cf6cd3115d14c8e266f1b51c2fd896136cc4
Matthew McKnight <matthewmcknight@Matthews-MBP.attlocal.net>
2020-09-22 14:06:18 -0500

    postEdit functionality made to be reviewed, postEdit css tied to page to be completed

--- client/src/screens/PostEdit/PostEdit.jsx
+++ client/src/screens/PostEdit/PostEdit.jsx
@@ -1,11 +1,90 @@
-import Layout from '../../components/shared/Layout/Layout'
-import React from 'react';
 
-const PostEdit = () => {
+import React, { useEffect, useState } from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import "./PostEdit.css";
+import { useParams, Redirect } from "react-router-dom";
+import { getPost, updatePost } from "../../services/posts";
+
+const PostEdit = (props) => {
+  const [post, setPost] = {
+    title: "",
+    imgURL: "",
+    content: "",
+  };
+
+  const [isUpdated, setUpdated] = useState(false);
+  let { id } = useParams();
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const post = await getPost(id);
+      setPost(post);
+    };
+  }, [id]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let { id } = props.match.params;
+    const updated = await updatePost(id, post);
+    setUpdated(updated);
+  };
+
+  if (isUpdated) {
+    return <Redirect to={`/posts/${props.match.params.id}`} />;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <Layout>
+      <div className="post=edit">
+        <div className="image-container">
+          <img
+            className="edit-post-image"
+            src={post.imgURL}
+            alt={`no information availiable`}
+          />
+          <form onSubmit={handleSubmit}>
+            <input
+              className="edit-input-image-link"
+              placeholder="Image Link"
+              value={post.imgURL}
+              name="imgURL"
+              required
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+        <form className="edit-form" onSubmit={handleSubmit}>
+          <input
+            className="input-name"
+            placeholder="Title"
+            value={post.title}
+            name="title"
+            required
+            autoFocus
+            onChange={handleChange}
+          />
+          <textarea
+            className="textarea-content"
+            rows={10}
+            cols={78}
+            placeholder="Content"
+            value={post.content}
+            name="content"
+            required
+            onChange={handleChange}
+          />
+          <button type="submit" className="save-button" />
+        </form>
+      </div>
+    </Layout>
   );
 };
 
