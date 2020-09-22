e75d0fffb6e9e963c6b720cd1aea4e93b62684cd
April Shenk <aprilshenk@MacBook-Pro.local>
2020-09-22 15:20:08 -0400

    fixed route

--- client/src/screens/PostCreate/PostCreate.jsx
+++ client/src/screens/PostCreate/PostCreate.jsx
@@ -1,11 +1,78 @@
-import Layout from '../../components/shared/Layout/Layout'
-import React from 'react';
+import Layout from "../../components/shared/Layout/Layout";
+import React, { useState } from "react";
+import { createPost } from "../../services/posts";
+import { Redirect } from "react-router-dom";
 
 const PostCreate = () => {
+  const [post, setPost] = useState({
+    title: "",
+    content: "",
+    imgUrl: "",
+    author: "",
+  });
+
+  const [isCreated, setCreated] = useState(false);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPost({
+      ...post,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const created = await createPost(post);
+  };
+
+  if (isCreated) {
+    return <Redirect to={`/posts`} />;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <Layout>
+      <form className='create-from' onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input-title"
+          placeholder="Title"
+          value={post.title}
+          name="title"
+          required
+          onChange={handleChange}
+        />
+        <textarea
+          type="text"
+          className="input-content"
+          rows={8}
+          placeholder="Content"
+          value={post.content}
+          name="content"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="link"
+          className="input-image-link"
+          placeholder='Image Link'
+          value={post.imgURL}
+          name='imgURL'
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="input-author"
+          placeholder="Author"
+          value={post.author}
+          name="author"
+          required
+          onChange={handleChange}
+        />
+        <button type="submit" className="submit-button">Sumbit</button>
+      </form>
+    </Layout>
   );
 };
 
