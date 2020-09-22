94b735b0e31ee8a597856e01154b8691afc700ef
April Shenk <aprilshenk@MacBook-Pro.local>
2020-09-22 11:34:32 -0400

    should be functional depenedent on components

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,1 +1,15 @@
-import Layout from '../../components/shared/Layout/Layout'
+import React from "react";
+import Layout from "../../components/shared/Layout/Layout";
+import PostCards from '../../components/PostCards/PostCards';
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className="home">
+        <PostCards />
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
